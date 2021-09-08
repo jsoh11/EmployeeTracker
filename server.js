5f7efecd2e9af1d702158503eb5df324adcbec0c
Jordan <jordansoh14@gmail.com>
2021-09-07 20:46:05 -0500

    added inquierer prompt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,9 +13,9 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    
+
     user: 'root',
-    
+
     password: '',
 
     database: 'tracker_db'
@@ -22,12 +23,32 @@ const db = mysql.createConnection(
   console.log(`Connected to the tracker_db database.`)
 );
 
+const promptUser = () => {
+
+  return inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['View all employees',
+                'Add employee',
+                'View all roles',
+                'Add role',
+                'View all Departments',
+                'Add departments',
+      ],
+      name: 'license',
+    },
+
+  ])
+}
+
+
 
 app.get('/api/department', ({ body }, res) => {
   const sql = `INSERT INTO DEPARTMENT (DEPARTMENT_NAME)
     VALUES (?)`;
   const params = [body.movie_name];
-  
+
   db.query(sql, params, (err, result) => {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -43,11 +64,11 @@ app.get('/api/department', ({ body }, res) => {
 
 app.get('/api/role', (req, res) => {
   const sql = `SELECT id, movie_name AS title FROM movies`;
-  
+
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
-       return;
+      return;
     }
     res.json({
       message: 'success',
@@ -58,11 +79,11 @@ app.get('/api/role', (req, res) => {
 
 app.get('/api/employee', (req, res) => {
   const sql = `SELECT id, movie_name AS title FROM movies`;
-  
+
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
-       return;
+      return;
     }
     res.json({
       message: 'success',
@@ -72,65 +93,7 @@ app.get('/api/employee', (req, res) => {
 });
 
 
-// app.delete('/api/movie/:id', (req, res) => {
-//   const sql = `DELETE FROM movies WHERE id = ?`;
-//   const params = [req.params.id];
-  
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.statusMessage(400).json({ error: res.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//       message: 'Movie not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'deleted',
-//         changes: result.affectedRows,
-//         id: req.params.id
-//       });
-//     }
-//   });
-// });
 
-// Read list of all reviews and associated movie name using LEFT JOIN
-// app.get('/api/movie-reviews', (req, res) => {
-//   const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
-//   db.query(sql, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });
-
-// Update review name
-// app.put('/api/review/:id', (req, res) => {
-//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-//   const params = [req.body.review, req.params.id];
-
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//     } else if (!result.affectedRows) {
-//       res.json({
-//         message: 'Movie not found'
-//       });
-//     } else {
-//       res.json({
-//         message: 'success',
-//         data: req.body,
-//         changes: result.affectedRows
-//       });
-//     }
-//   });
-// });
-
-// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
