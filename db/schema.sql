4102538a65ba748ee467dc4770b95fe7c137b5d6
Jordan <jordansoh14@gmail.com>
2021-10-10 20:41:40 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,11 +8,14 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT NOT NULL
+CREATE TABLE employee_role (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -21,4 +24,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES emp_role(id)
+    ON DELETE SET NULL,
 );
