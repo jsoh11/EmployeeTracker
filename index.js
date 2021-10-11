2b0cb5a70c4a870b55566e65e7227f15108d4113
Jordan <jordansoh14@gmail.com>
2021-10-11 11:40:35 -0500

    added table functions

--- index.js
+++ index.js
@@ -49,3 +49,43 @@ inquirer.prompt(
       quit(res);
       break;
   }})}
+
+  async function depTable(data){
+    const showDbQuery =  new Department()
+    showDbQuery.showDep(data)
+    console.log('\n')
+    init();
+}
+
+
+async function roleTable(data){
+    const showDbQuery = new Role()
+    showDbQuery.showRole(data)
+    console.log('\n')
+    init();
+}
+
+
+async function empTable(data){
+    const showDbQuery = new Employee()
+    showDbQuery.showEmp(data)
+    console.log('\n')
+    init();
+}
+
+
+function addDepartment(data){
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'dep_name',
+        message: "What's the name of the department?",
+      },
+    ])
+    .then((data) => {
+      const addTo = new Department()
+      addTo.addDep(data.dep_name)
+      console.log(`Added ${data.dep_name} to the database!`);
+      init();
+    })
+  }
